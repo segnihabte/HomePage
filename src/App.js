0c2055b73dad9c0e821f9335879f77994940c384
segni <segnih36@gmail.com>
2022-05-25 16:22:50 +0300

    web link routing correction

--- src/App.js
+++ src/App.js
@@ -18,10 +18,10 @@ function App() {
         <Container>
           <Routes>
             <Route path="/Port1" element={<Body />}></Route> 
-            <Route path="/Service" element={<Service/>}></Route>
-            <Route path="/contact" element={<Contact/>}></Route>
-            <Route path="/Resume" element={<Resume/>}></Route>
-            <Route path="/About" element={<About/>}></Route>
+            <Route path="/Port1/Service" element={<Service/>}></Route>
+            <Route path="/Port1/contact" element={<Contact/>}></Route>
+            <Route path="/Port1/Resume" element={<Resume/>}></Route>
+            <Route path="/Port1/About" element={<About/>}></Route>
           </Routes>
         </Container>
       </main>
